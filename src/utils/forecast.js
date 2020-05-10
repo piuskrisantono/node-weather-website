3bbb890536ef39aa38203c27dd22ab3dcac47f14
piuskrisantono <pius.krisantono@gmail.com>
2020-05-10 14:03:45 +0700

    Add new forecast data (humidity)

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request")
 
 const forecast = (latitude, longitude, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=6d7881dfccf1370db94bb97cd470872c&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "&units=f"
+    const url = "http://api.weatherstack.com/current?access_key=6d7881dfccf1370db94bb97cd470872c&query=" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude)
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Forecast service is unavailble.", undefined)
@@ -10,8 +10,9 @@ const forecast = (latitude, longitude, callback) => {
         } else {
             callback(undefined,
                 body.current.weather_descriptions[0] + ". It is currently " +
-                body.current.temperature + " degrees out. It feels like " +
-                body.current.feelslike + " degrees."
+                body.current.temperature + " degrees Celcius out. It feels like " +
+                body.current.feelslike + " degrees. It is now " +
+                body.current.humidity + "% on humidity."
             )
         }
     })
